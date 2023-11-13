7095ebb766e82cc62b831848562b2ce738df9f4e
Amr Mousa <amrmousa1682@gmail.com>
2023-11-13 22:10:15 +0200

    task 2

--- login.c
+++ login.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct
 {
     char username[20];
     char password[20];
+    bool isActived;
 } typedef user;
 
 struct LinkedList
@@ -51,13 +53,29 @@ void addUser()
 {
     char username[20];
     char password[20];
+    char isActived[20];
     printf("Enter username: ");
     scanf("%s", username);
     printf("Enter password: ");
     scanf("%s", password);
+    printf("Enter activation status: ");
+    scanf("%s", isActived);
     user user;
     strcpy(user.username, username);
     strcpy(user.password, password);
+    if (strcmp(isActived, "true") == 0 || strcmp(isActived, "1") == 0)
+    {
+        user.isActived = true;
+    }
+    else if (strcmp(isActived, "false") == 0 || strcmp(isActived, "0") == 0)
+    {
+        user.isActived = false;
+    }
+    else
+    {
+        printf("Invalid activation status!\n");
+        return;
+    }
     head = addNode(head, user);
     printf("Register successfully!\n");
 }
@@ -75,6 +93,11 @@ void login()
     {
         if (strcmp(p->data.username, username) == 0 && strcmp(p->data.password, password) == 0)
         {
+            if(p->data.isActived == false)
+            {
+                printf("Account is not activated!\n");
+                return;
+            }
             printf("Login successfully!\n");
             return;
         }
